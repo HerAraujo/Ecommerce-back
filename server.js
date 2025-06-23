23be7162596360da5b01eef88aaabfd25b535ea2
Hernan Araujo <hernan.araujo@cencosud.com.uy>
2025-06-23 10:38:07 -0300

    Implement CORS configuration and add .env.example and .gitignore files

--- server.js
+++ server.js
@@ -7,9 +7,24 @@ const cors = require("cors");
 const APP_PORT = process.env.APP_PORT || 8001;
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:3000"
+];
+
 //Middlewares
 app.use(express.static("public"));
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Métodos permitidos
+  allowedHeaders: ["Content-Type", "Authorization"] // Encabezados permitidos
+}));
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 
