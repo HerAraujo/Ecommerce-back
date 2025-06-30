9ebdf53dfed79a6fe44874b14900c54c2eee6718
Hernan Araujo <hernan.araujo@cencosud.com.uy>
2025-06-29 23:03:15 -0300

    Add allowed origin for CORS configuration to support admin panel access

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://crafterbeer.vercel.app"
+  "https://crafterbeer.vercel.app",
+  "https://crafterbeer-admin.vercel.app"
 ];
 
 //Middlewares
